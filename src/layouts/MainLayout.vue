d44485d86d4e8cb91f7f9d624f5bea756a65e75c
n.yoshida <yoshida@polaris.works>
2025-06-22 14:06:51 +0900

    add src-tauri

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,57 +2,42 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title>
-          Nariwai
-        </q-toolbar-title>
+        <q-toolbar-title> Nariwai</q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label header>
-          Essential Links
-        </q-item-label>
+        <q-item-label header> Essential Links</q-item-label>
 
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
+        <EssentialLink v-for="link in linksList" :key="link.title" v-bind="link" />
       </q-list>
     </q-drawer>
 
     <q-page-container>
-      <router-view/>
+      <router-view />
     </q-page-container>
   </q-layout>
 </template>
 
 <script setup lang="ts">
-import {ref} from 'vue';
-import EssentialLink, {type EssentialLinkProps} from 'components/EssentialLink.vue';
+import { ref } from 'vue';
+import EssentialLink, { type EssentialLinkProps } from 'components/EssentialLink.vue';
 
 const linksList: EssentialLinkProps[] = [
   {
     title: 'Docs',
     caption: 'quasar.dev',
+    icon: 'matMenu',
+  },
+  {
+    title: 'Package',
+    caption: 'quasar.dev',
     icon: 'school',
-    link: 'https://quasar.dev'
   },
 ];
 
